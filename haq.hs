27cb3efe99ada6ca3eed7863ff00481d9b86580a
Maximilian Meyer <maximilian.mey@googlemail.com>
2015-12-27 13:13:57 +0100

    testing

--- haq.hs
+++ haq.hs
@@ -1,15 +1,12 @@
-cat > ffp.hs
-
+$ cat > Haq.hs
 --
--- Copyright (c) 2015 Maximilian Meyer
+-- Copyright (c) 2006 Don Stewart - http://www.cse.unsw.edu.au/~dons/
 -- GPL version 2 or later (see http://www.gnu.org/copyleft/gpl.html)
-
+--
 import System.Environment
  
 -- | 'main' runs the main program
 main :: IO ()
 main = getArgs >>= print . haqify . head
  
-haqify s = "FFP! " ++ s
-
--- new branch
+haqify s = "Haq! " ++ s
