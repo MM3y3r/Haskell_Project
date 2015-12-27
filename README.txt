f5bed143a9978705cab57082b5f022547147ba87
Maximilian Meyer <maximilian.mey@googlemail.com>
2015-12-27 12:45:59 +0100

    setting up basic structure

---
+++ README.txt
@@ -0,0 +1,6 @@
+Welcome to this project
+
+by
+
+Maximilian
+Georg
---
+++ ffp.hs
@@ -0,0 +1,13 @@
+cat > ffp.hs
+
+--
+-- Copyright (c) 2015 Maximilian Meyer
+-- GPL version 2 or later (see http://www.gnu.org/copyleft/gpl.html)
+
+import System.Environment
+ 
+-- | 'main' runs the main program
+main :: IO ()
+main = getArgs >>= print . haqify . head
+ 
+haqify s = "Haq! " ++ s
